9c70e474dea9d749c1228b87e30fb99f285c7ae2
kevin <liuenbofuture@foxmail.com>
2024-11-21 19:02:12 +0800

    opt: fix

--- app.bat
+++ app.bat
@@ -15,7 +15,7 @@ set node_num=2
     docker run -d --name ambari-repo  --network ambari_cluster_net  --add-host kaq.kj.com:127.0.0.1 --ip 172.188.0.2 -it  tungshuaishuai/ambari-repo:2.7.6.3
 
     echo "init init-hosts.sh..."
-    replace "node_num=.*" node_num=$node_num init-hosts.sh
+    replace "node_num=2" node_num=$node_num init-hosts.sh
 
 
     echo "create ambari-server..."
@@ -23,7 +23,7 @@ set node_num=2
     docker cp init-hosts.sh         amb-server:/root/
     docker cp init-ambari-server.sh amb-server:/root/
 
-    for %%i in (0, %node_num%) do (
+    for /L %%i in (0, %node_num%) do (
         echo "create ambari-agent"%i%
         set /a a = 30+%i%;
         docker run -d --privileged --name  amb%i%   --network ambari_cluster_net  --add-host kaq.kj.com:127.0.0.1 --ip 172.188.0.%a% -it  tungshuaishuai/ambari-node:2.7.6.3
