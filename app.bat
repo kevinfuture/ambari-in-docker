7e3550fa67739884d0f496bb48debd83357e66c5
kevin <liuenbofuture@foxmail.com>
2024-11-21 18:15:21 +0800

    opt: test

--- app.bat
+++ app.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001
-set /p input=Please enter your selection 
+set /p input=Please enter your selection :
 
 docker pull tungshuaishuai/ambari-repo:2.7.6.3
 docker pull tungshuaishuai/ambari-node:2.7.6.3
@@ -8,29 +8,30 @@ docker pull tungshuaishuai/ambari-node:2.7.6.3
 set node_num=2
 
 :create
-    echo "创建网络"
+    echo "create network..."
     docker network create --subnet=172.188.0.0/16 ambari_cluster_net
 
-    echo "启动repo"
+    echo "start repo..."
     docker run -d --name ambari-repo  --network ambari_cluster_net  --add-host kaq.kj.com:127.0.0.1 --ip 172.188.0.2 -it  tungshuaishuai/ambari-repo:2.7.6.3
 
-    echo "初始化脚本init-hosts.sh"
+    echo "init init-hosts.sh..."
     replact "node_num=.*" node_num=$node_num init-hosts.sh
 
 
-    echo "创建ambari-server"
+    echo "create ambari-server..."
     docker run -d --privileged --name amb-server   --network ambari_cluster_net --add-host kaq.kj.com:127.0.0.1 --ip 172.188.0.3 -p 8181:8080 -it  tungshuaishuai/ambari-node:2.7.6.3
     docker cp init-hosts.sh         amb-server:/root/
     docker cp init-ambari-server.sh amb-server:/root/
 
     for %%i in (0, %node_num%) do (
-        echo "创建ambari-agent$i"
-        docker run -d --privileged --name  amb$i   --network ambari_cluster_net  --add-host kaq.kj.com:127.0.0.1 --ip 172.188.0.$((3+$i)) -it  tungshuaishuai/ambari-node:2.7.6.3
+        echo 创建ambari-agent%i%
+        set /a a = 30+%i%;
+        docker run -d --privileged --name  amb$i   --network ambari_cluster_net  --add-host kaq.kj.com:127.0.0.1 --ip 172.188.0.%a% -it  tungshuaishuai/ambari-node:2.7.6.3
         docker cp init-hosts.sh    amb$i:/root/
     )
 
 
-    echo "执行init-hosts"
+    echo "do init-hosts..."
     docker exec -it amb-server bash /root/init-hosts.sh
     docker exec -it amb-server wget http://repo.hdp.link/ambari/centos7/2.7.6.3-2/ambari.repo -P /etc/yum.repos.d/
     docker exec -it amb-server wget http://repo.hdp.link/HDP/centos8/3.3.1.0-002/hdp.repo -P /etc/yum.repos.d/
@@ -41,13 +42,13 @@ set node_num=2
     )
 
 
-    echo "初始化ambari-server"
+    echo "init ambari-server"
     docker exec -it amb-server bash /root/init-ambari-server.sh
 exit /b
 
 
 :start
-    echo "执行start"
+    echo "start!"
     docker start amb-server
 
     for %%i in (0, %node_num%) do (
@@ -66,7 +67,7 @@ exit /b
 
 
 :stop
-    echo "执行stop"
+    echo "stop!"
     docker stop amb-server
     for %%i in (0, %node_num%) do (
         docker stop amb$i
